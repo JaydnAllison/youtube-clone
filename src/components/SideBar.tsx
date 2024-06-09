4b4a0ef3b251901dbfa5e6366521f8371bf12a61
JaydnAllison <jaydn.allison@gmail.com>
2024-06-09 13:07:51 +0100

    Added the logo to the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import youtubeLogo from '../assets/YoutubeLogo.png';
+
 type SideBarProps = {
     toggleSideBar: () => void
 };
@@ -5,9 +8,12 @@ type SideBarProps = {
 export default function SideBar({toggleSideBar}: SideBarProps): JSX.Element {
     return (
         <div className='sidebar-container'>
-            <div className='sidebar-container--toggle-sidebar-button'>
+            <div className='sidebar-container--navigation'>
+                <div className='sidebar-container--toggle-sidebar-button'>
                     <button onClick={toggleSideBar}><i className="bi bi-three-dots-vertical"></i></button>
                 </div>
+                <Link to='/' className='youtube-logo'><img src={youtubeLogo} width={90}/></Link>
+            </div>
         </div>
     )
 }
